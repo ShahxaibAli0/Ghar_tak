26e063211163f985a0794eefa5b21575ccb76c41
shahzaibali0 <117452146+shahzaibali0@users.noreply.github.com>
2026-03-02 01:22:32 +0500

    main file fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
 import 'screens/signup_screen.dart';
-import 'screen/home_screen.dart';
+import 'home_screen.dart';
 
 void main() {
   runApp(GharTakApp());
